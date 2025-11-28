c0e872423a65ae5038fc0677b6b15e428dbf545c
Ahmed-Alsefari <aalsefari0007@stu.kau.edu.sa>
2025-11-28 23:52:34 +0300

    Add PasswordEncoder and RequiredArgsConstructor imports

--- app/src/main/java/ems/app/service/UserService.java
+++ app/src/main/java/ems/app/service/UserService.java
@@ -3,8 +3,10 @@ package ems.app.service;
 
 import ems.app.model.User;
 import ems.app.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 
@@ -33,4 +35,5 @@ public class UserService {
         return userRepository.save(user);
     }
 }
+
 
